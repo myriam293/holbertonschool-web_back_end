e91ddca1b7254b9644b4c04a10be9935985c1237
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-13 16:34:45 +0100

    task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Type-annotated function add that returns the sum of two floats."""
 
+
 def add(a: float, b: float) -> float:
     """Return the sum of a and b."""
     return a + b
