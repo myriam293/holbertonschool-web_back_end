149abf3a792c212f508c471d1df6a1583aeebfe3
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-13 19:16:31 +0100

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""Module for function that creates a tuple with a string and the square of a number."""
+"""Module for function that creates a tuple with
+a string and the square of a number."""
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return a tuple where the first element is k and the second is v squared as a float."""
+    """Return a tuple where the first element
+    is k and the second is v squared as a float."""
     return k, float(v ** 2)
