a43dbb950d173eefc88d7186efd845f0c3ada709
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-13 16:54:08 +0100

    task 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Type-annotated function to_str that returns the string representation of a float."""
+"""Type-annotated function to_str that returns
+the string representation of a float."""
 
 def to_str(n: float) -> str:
     """Return the string representation of n."""
