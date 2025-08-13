766bf28caf400a4b0df19690b192b3b405a28a39
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-13 19:11:16 +0100

    task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """Module for function that finds the sum of a mixed list."""
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Return the sum of a mixed list."""
     return sum(mxd_lst)
