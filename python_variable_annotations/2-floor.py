e8c5ede74da4f0979121d534cb09867488b7c7cc
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-13 16:47:00 +0100

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Type-annotated function floor that
+returns the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of n."""
+    return math.floor(n)
