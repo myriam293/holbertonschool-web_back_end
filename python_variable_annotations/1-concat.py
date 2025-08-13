ee065de290b15ea306463e06d03fc1993246ed03
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-13 16:40:30 +0100

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function concat that returns
+the concatenation of two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of str1 and str2."""
+    return str1 + str2
