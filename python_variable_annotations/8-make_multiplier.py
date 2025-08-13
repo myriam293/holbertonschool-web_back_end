8eb755661f62d5e8ff272c44cc0c8008959e7190
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-08-13 19:19:45 +0100

    task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for function that returns a multiplier function."""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies a float by the given multiplier."""
+    def func(x: float) -> float:
+        return x * multiplier
+    return func
